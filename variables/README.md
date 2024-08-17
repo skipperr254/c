1209074da817042a3fe227d6c1e3695c69d45bbe
skipperr254 <skipperr254@gmail.com>
2024-08-17 17:36:56 +0300

    variables in C

---
+++ variables/README.md
@@ -0,0 +1,9 @@
+## Key Takeaways
+* Variable declaration
+* Variable definition
+* Variable initialization
+* Variable assignment and re-assignment
+* Newline operator
+* String modifiers
+* Variable naming conventions
+* Case sensitivity
---
+++ variables/variables.c
@@ -0,0 +1,15 @@
+// Variables in C
+
+#include <stdio.h>
+
+int main() {
+	int num1; // variable declaration
+	num1 = 10; // variable definition
+	int num2 = 20; // variable initilization
+	printf("%d\n", num1); // prints 10
+	printf("%d\n", num2); // prints 20
+	num1 = 15; // variable re-assignment
+	printf("%d\n", num1); // prints 15 
+	
+	return (0);
+}
